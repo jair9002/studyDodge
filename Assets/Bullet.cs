443b815d742cbb0eeb7ccc43fe68ce1f340d7465
OhCheolWon <cwoh9002@naver.com>
2021-03-09 14:33:50 +0900

    second

---
+++ Assets/Bullet.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    public float speed = 8f;
+    private Rigidbody bulletRigidbody;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //게임 오브젝트에서 리지드바디 컴포넌트를 찾아 bulletRigidbody 에 할당
+        bulletRigidbody = GetComponent<Rigidbody>();
+        // 리지드바디의 속도 = 앞쪽 방향 * 이동 속력
+        bulletRigidbody.velocity = transform.forward * speed;
+
+        //transform 컴포넌트는 게임 오브젝트의 위치,크기 회전을 담당하는 컴포넌트
+        //모든 게임 오브젝트가 하나씩 가지고 있도록 강제됨
+        //forward 변수로 게임 오브젝트의 앞쪽 방향 알 수 있었습니다.
+
+        Destroy(gameObject, 3f); //3초 뒤에 파괴 두번째 파라미터가 시간을 의미
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    // OnCollision 계열 함수는 일반적인 콜라이더 가진 두 게임 오브젝트가 충돌할때 자동실행 서로 밀쳐냄
+    // OnTrigger 둘 중 하나라도 트리거 콜라이더라면 충돌시 서로 통과
+
+    private void OnTriggerEnter(Collider other)
+    {
+        //충돌한 상대방 게임 오브젝트가 Player 태그를 가진경우
+        if (other.tag == "Player")
+        {
+            //상대방 게임 오브젝트에서 PlayerController 컴포넌트 가져오기
+            PlayerController playerController = other.GetComponent<PlayerController>();
+
+            //상대방 PlayerController 컴포넌트 가져왔다면
+            if(playerController != null)
+            {
+                //다이 실행
+                playerController.Die();
+            }
+        }
+    }
+}
---
+++ Assets/BulletSpawner.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletSpawner : MonoBehaviour
+{
+    public GameObject bulletPrefab;
+    public float spawnRateMin = 0.5f; //최소 생성 주기
+    public float spawnRateMax = 3f; // 최대 생성 주기
+
+    private Transform target; //발사할 대상
+    private float spawnRate; //생성 주기
+    private float timeAfterSpawn; //최근 생성 시점에서 지난 시간
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //최근 생성 이후의 누적 시간을 0으로 초기화
+        timeAfterSpawn = 0f;
+        //탄알 생성 간격을 spawnrateMin 과 spawnRateMax 사이에서 랜덤 지정
+        spawnRate = Random.Range(spawnRateMin, spawnRateMax);
+        // PlayerController 컴포넌트를 가진 게임 오브젝트를 찾아 조준 대상으로 설정 
+        // FindObjectOfType 씬에 있는 모든 오브젝트 조사해서 <> 안에 있는 대상 찾아냄
+        target = FindObjectOfType<PlayerController>().transform;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //1초에 60번 실행되는 update에 맞춰 시간 계산을 하는 함수 deltaTime은 1/60 의 값을 가진다
+        timeAfterSpawn += Time.deltaTime;
+    
+        if (timeAfterSpawn >= spawnRate)
+        {
+            timeAfterSpawn = 0f; //리셋
+
+            //Instantiate 원본 오브젝트를 복제하여 생성하는 메소드
+            // Instantiate(원본,위치,회전)
+            GameObject bullet = Instantiate(bulletPrefab, transform.position, transform.rotation);
+
+            //Lookat은 게임 오브젝트의 정면 방향이 target을 향하도록 지정 파라매터형식은 transform
+            bullet.transform.LookAt(target);
+
+            //다음번 생성 간격을 Min, Max사이값으로 랜덤 지정
+            spawnRate = Random.Range(spawnRateMin, spawnRateMax);
+
+        }
+    
+    }
+}
